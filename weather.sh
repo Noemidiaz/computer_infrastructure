ba1729eb52b12b52362c2886d5433cd417ce5633
Pilar Noemi Diaz <158086124+Noemidiaz@users.noreply.github.com>
2024-11-30 20:08:16 +0000

    run script

--- weather.sh
+++ weather.sh
@@ -1,3 +1,8 @@
 #! /bin/bash
 
-wget -O data/weather/ `date +"%Y%m%d_%H%M%S_athenry.json"` https://prodapi.metweb.ie/observations/athenry/today
+date
+echo "Downloading weather data"
+wget -O data/weather/ `date +"%Y%m%d_%H%M%S_athenry.json"`https://prodapi.metweb.ie/observations/athenry/today  
+wget -O data/weather/$(date +"%Y%m%d_%H%M%S_athenry.json") https://prodapi.metweb.ie/observations/athenry/today
+echo "weather data download"
+date
