2b52ba7e670fcf91a39fdfda9c4f8ba641b26cd2
Pilar Noemi Diaz <158086124+Noemidiaz@users.noreply.github.com>
2024-12-18 22:48:09 +0000

    description steps task 6 and 7

--- weather.sh
+++ weather.sh
@@ -2,7 +2,8 @@
 
 date
 echo "Downloading weather data"
-wget -O data/weather/ `date +"%Y%m%d_%H%M%S_athenry.json"`https://prodapi.metweb.ie/observations/athenry/today  
-wget -O data/weather/$(date +"%Y%m%d_%H%M%S_athenry.json") https://prodapi.metweb.ie/observations/athenry/today
+wget -O data/weather/ `date +"%Y%m%d_%H%M%S"_athenry.json"`https://prodapi.metweb.ie/observations/athenry/today  
+wget -O data/weather/$(date +"%Y%m%d_%H%M%S_athenry.json") https://prodapi.metweb.ie/observations/athenry/today 
 echo "weather data download"
 date
+
